613220920082bc00642fe6ab77692a3921e7a247
doug <doug@Doug-PC>
2015-12-07 21:10:06 -0300

    Revert "Test2 Commit "
    This reverts commit e043759a41e3e00e5cf756505aeb3de65e871ca6.

--- AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
+++ AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
@@ -16,7 +16,7 @@ public class AnalisadorLexico {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hey there");
+        System.out.println("Hello World");
     }
     
 }
