92c68b354ccb6153a7dd59ae748465c9066f0c8f
doug <doug@Doug-PC>
2015-12-13 16:34:48 -0300

    Cadeia Constante funcionando

--- AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
+++ AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
@@ -5,6 +5,11 @@
  */
 package analisadorlexico;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author doug
@@ -14,9 +19,75 @@ public class AnalisadorLexico {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
-        System.out.println("Hello World");
+        
+        BufferedReader input = new BufferedReader(new FileReader("input.txt")); 
+        int symbol = 0; 
+        boolean eof = false;
+        int err_count = 0;
+        int pos [] = {1, 0};
+        String token = "";
+        boolean accept = true;
+        
+        
+        while (!eof) {
+            token = "";
+            accept = true;
+            
+            symbol = input.read();
+            pos [1]++;
+            
+            if(symbol != '\n' && symbol != -1)
+            token = token + (char)symbol;
+
+            switch (symbol){
+
+                case '"':
+                    while((symbol = input.read()) != '"' && symbol != '\n' && symbol != -1){
+                        token = token + (char) symbol;
+                        if(symbol > 126 || symbol < 32) {
+                            System.out.println("Erro: Cadeia mal formada (Símbolo Ínvalido)");
+                            accept = false;
+                            err_count ++;
+                        }
+                    }
+                    
+                     if(symbol=='"'){
+                        token = token + (char) symbol;
+                        if(accept == true)
+                        System.out.println(token);
+                    } else { //if(symbol == -1 || symbol == '\n')
+                        System.out.println("Erro: Cadeia constante não terminada");
+                        err_count ++;
+                    } 
+                        
+                    break;
+                
+                case -1:
+                    System.out.println("Numero de erros: "+err_count);
+                    eof = true;
+                break;
+
+                
+
+            }
+               
+                    
+                    
+                    
+                
+                
+                
+           
+        
+        } 
+        
+        input.close();
+
+   
+        
+        
     }
     
 }
