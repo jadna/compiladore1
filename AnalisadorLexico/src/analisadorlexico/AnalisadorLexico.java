c36004295efb115f0b8e5b53eecc9c037b4dd839
doug <doug@Doug-PC>
2015-12-13 19:48:20 -0300

    If no lugar do switch case

--- AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
+++ AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
@@ -16,22 +16,39 @@ import java.io.IOException;
  */
 public class AnalisadorLexico {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+        private BufferedReader input; 
+        private int symbol = 0; 
+        private boolean eof = false;
+        private int err_count = 0;
+        private int pos [];
+        private String token = "";
+        private boolean accept = true;
+    
+    public AnalisadorLexico() throws FileNotFoundException{
+        input = new BufferedReader(new FileReader("input.txt"));
+        symbol = 0; 
+        eof = false;
+        err_count = 0;
+        pos = new int[2];
+        pos[0] = 1;
+        pos[1] = 0;
+        token = "";
+        accept = true;
+        
+    }
+        
+        
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
         
-        BufferedReader input = new BufferedReader(new FileReader("input.txt")); 
-        int symbol = 0; 
-        boolean eof = false;
-        int err_count = 0;
-        int pos [] = {1, 0};
-        String token = "";
-        boolean accept = true;
+        AnalisadorLexico AnLex = new AnalisadorLexico();
+        AnLex.Automato();
         
-        
-        while (!eof) {
+    }
+    
+    public void Automato() throws IOException{
+         while (!eof) {
             token = "";
             accept = true;
             
@@ -40,11 +57,41 @@ public class AnalisadorLexico {
             
             if(symbol != '\n' && symbol != -1)
             token = token + (char)symbol;
+            
+                
+                    if(symbol == '"'){
+                        CadeiaConst();
+                     
+                    } 
+                    
+                    
+                    else if(symbol == '\''){
+                        CaractereConst();
+                    
+                    }
+                    
+                    else if(Character.isLetter(symbol)){
+                        
+                    }
+                
+                
+                    else if(symbol == -1){
+                    System.out.println("Numero de erros: "+err_count);
+                    eof = true;
+                    }
 
-            switch (symbol){
-
-                case '"':
-                    while((symbol = input.read()) != '"' && symbol != '\n' && symbol != -1){
+                
+        
+        } 
+        
+        input.close();
+        
+    }
+    
+    
+    public void CadeiaConst() throws IOException{
+        
+         while((symbol = input.read()) != '"' && symbol != '\n' && symbol != -1){
                         token = token + (char) symbol;
                         if(symbol > 126 || symbol < 32) {
                             System.out.println("Erro: Cadeia constante mal formada (Símbolo Ínvalido)");
@@ -63,12 +110,12 @@ public class AnalisadorLexico {
                      
                      if(accept == true)
                         System.out.println(token);
-                     
-                    break;
-                
-                case '\'':
-                    symbol = input.read();
-                    if ((symbol >= 48 && symbol <= 57) || (symbol >= 65 && symbol <= 90) || (symbol >= 97 && symbol <= 122)) {
+        
+    }
+    
+    public void CaractereConst() throws IOException{
+        symbol = input.read(); // (symbol >= 48 && symbol <= 57) || (symbol >= 65 && symbol <= 90) || (symbol >= 97 && symbol <= 122)
+                    if ((Character.isLetter(symbol))||(Character.isDigit(symbol))) {
                         token = token + (char) symbol;
                     } else if (symbol == -1 || symbol == '\n'){
                         System.out.println("Erro: Caractere constante terminado incorretamente");
@@ -92,33 +139,6 @@ public class AnalisadorLexico {
                     
                     if(accept == true)
                         System.out.println(token);
-                    
-                    break;
-                
-                case -1:
-                    System.out.println("Numero de erros: "+err_count);
-                    eof = true;
-                break;
-
-                
-
-            }
-               
-                    
-                    
-                    
-                
-                
-                
-           
-        
-        } 
-        
-        input.close();
-
-   
-        
-        
     }
     
 }
